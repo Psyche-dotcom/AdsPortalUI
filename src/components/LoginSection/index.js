7eab2076bad01d4392e2c0d87551a74e5fc7c89c
Saheed Olawale Babatunde <53710927+Psyche-dotcom@users.noreply.github.com>
2024-06-13 08:57:40 +0100

    meta and face page

--- src/components/LoginSection/index.js
+++ src/components/LoginSection/index.js
@@ -65,7 +65,7 @@ const LoginSection = () => {
             </button>
           </form>
           <p className="mt-4 text-center">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/register" className="text-blue-500">
               Register
             </Link>
